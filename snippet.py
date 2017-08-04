47d06d766f646481b80a71b4aefa0078d605caac
Raymond Blum <raygeeknyc@gmail.com>
2017-08-03 22:44:38 -0700

    Fixed up code, added image results specifier to search call. Tested, working.

--- snippet.py
+++ snippet.py
@@ -1,13 +1,19 @@
-import ../authinfo
+import pprint
+from googleapiclient.discovery import build
+import authinfo
+
 def main():
   # Build a service object for interacting with the API. Visit
   # the Google APIs Console <http://code.google.com/apis/console>
   # to get an API key for your own application.
   service = build("customsearch", "v1",
-            developerKey=authinfo.developer_key
+            developerKey=authinfo.developer_key)
 
   res = service.cse().list(
-      q='tuna`',
+      q='pretty bird',
+      searchType = "image",
       cx=authinfo.ctx
     ).execute()
   pprint.pprint(res)
+
+main()
